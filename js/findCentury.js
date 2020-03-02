7a2e1fadb2ef28a2702b6f6d6c4db1803166a8eb
anhtu6 <anhtu632@gmail.com>
2020-03-02 14:20:59 +0700

    updated

--- js/findCentury.js
+++ js/findCentury.js
@@ -1,7 +1,8 @@
+let input = parseInt(prompt('Nhap nam can kiem tra'));
 function findCentury(year) {
     let n= Math.floor(year/100);
     if(year%100==0) return document.write('nam '+year+' thuoc the ki '+n);
 
     return document.write('nam '+year+' thuoc the ki '+(n+1))
 }
-findCentury(200);
+findCentury(input);
